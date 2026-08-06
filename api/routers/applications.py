c542ad2d6379dddc89b407b0f581719070e9a1b4
Moses Avila <mosesavila9@gmail.com>
2026-08-06 00:22:02 -0700

    pass fact_order to render calls

--- api/routers/applications.py
+++ api/routers/applications.py
@@ -141,7 +141,9 @@ def preview_application(
     master = _load_master(session, db)
     selections = {k: v.model_dump(exclude_none=True) for k, v in body.selections.items()}
     try:
-        tex = core_bridge.render_tex(master, _tailored_from(version), selections)
+        tex = core_bridge.render_tex(
+            master, _tailored_from(version), selections, body.fact_order
+        )
     except ValueError as e:
         raise ApiError(409, "stale_tailored_resume", str(e)) from e
     return RenderPreviewResponse(tex=tex)
@@ -158,7 +160,7 @@ def finalize_application(
     selections = {k: v.model_dump(exclude_none=True) for k, v in body.selections.items()}
     try:
         tex, pdf_path, log = core_bridge.render_and_compile(
-            master, _tailored_from(version), selections
+            master, _tailored_from(version), selections, body.fact_order
         )
     except ValueError as e:
         raise ApiError(409, "stale_tailored_resume", str(e)) from e
